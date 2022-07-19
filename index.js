d82fa5416152e020f6553ede3b44fbf51968ae94
naimulislamshakil <ms0413354@gmail.com>
2022-07-19 06:54:06 +0600

    add mongodb

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.iuwr8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+  } finally {
+    // await client.close()
+  }
+};
+
 app.get("/", (req, res) => {
   res.send("How are you?");
 });
